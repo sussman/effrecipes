016fe03386976a1c85a0c7dd47d597d9ac5de2a9
sussman <sussman@183c59d2-f21e-0410-bca8-81a7b2b58be6>
2003-06-21 15:49:08 +0000

    * init-tables.sql:  make sure unique keys have AUTO_INCREMENT.
    * recipes.py:  start fleshing out real code!

--- trunk/recipes.py
+++ trunk/recipes.py
@@ -1,55 +1,98 @@
 #!/usr/bin/env python2
 
+# (C) 2003 Ben Collins-Sussman under the GNU General Public License.
+# See the file COPYING for details.
+
 # A set of python classes which "encapsulate" the data objects being
 # manipulated in the effrecipe database.  The fields of each class
 # match the object being manipulated.  Each class has methods for
 # reading/writing the object to the database.
 
+# Create "in memory' objects... what are the use-cases?
+#
+#  - READ: user does a query, gets back a list of objects
+#  - MODIFY: edits one object's fields, wants to overwrite existing record.
+#  - CREATE: fills in all fields (but id), new record written & id created.
+#
+#  READ:    1. construct query, get back list of rows
+#           2. convert each row into an object (all fields already present)
+#           3. present list to user, each hyperlinked with an Id
+#           4. create an object based on Id alone (lookup), render.
+#
+#  MODIFY:  1. start with an object, render fields for user
+#           2. user edits fields (but Id is not editable)
+#           3. object constructed from all fields, re-saved under same Id-key.
+#
+#  CREATE:  1. bunch of empty fields presented to user (not Id)
+#           2. object constructed from all fields
+#           3. save object as 'new' --> new Id created.
+
+import MySQLdb
+
+
+# ------ UNITS -------------------------------------------------------
 
 # a basic unit of measure.  the db table is just a name and unique id.
 class Unit:
 
-  def __init__(self, Name, UnitId=-1):
+  # Constructor:  all fields already available, just assemble.
+  # Pass "None" for Id if this is a new object, not yet saved.
+  def __init__(self, Id, Name):
+    self.UnitId = Id
     self.Name = Name
-    if UnitId == -1:
-      # if user didn't pass an Id, try to look one up based on name
-      self.UnitId = self.id_from_name(self, Name)
 
-  def id_from_name(self, Name):
-    """Look up the name of the unit in the db, return unique id
-    number.  If not found, return -1."""
+  def save(self, connection):
+    """Save object to database, keyed on its Id field.  If object's Id
+    field is defined, then overwrite existing record.  Else create new
+    record for Id."""
 
-  def write_new_unit(self, Name):
-    """Write a new unit kind to the db, return new unique id."""
+    if (self.UnitId):
+      fields = "Name=" + self.Name      
+      command = "UPDATE units SET " + fields + " WHERE UnitId=" + str(Id), ";"
+
+    else:
+      # by not mentioning the Id field, it should auto-increment.
+      fields = "(Name)"
+      values = "(" + "'" + self.Name + "'" + ")"
+      command = "INSERT INTO units " + fields + " VALUES " + values + ";"
+
+    cursor = connection.cursor()
+    cursor.execute(command)  ### return error if failure?
+
+  def delete(self, connection):
+    """Delete object from database, keyed on its Id field."""
+
+    ### DELETE FROM table WHERE Id==self.UnitId.
 
 
-# utility function:  get list of ALL unique ingredients in db
-def get_all_ingredients():
-  pass
-  ### run a sql query, build an Ingredient object for each hit.
+# General 'read' interfaces which return object(s)
 
+def unit_lookup(Id, connection):
+    "Construct object by looking it up its ID."
+
+    query = "SELECT * FROM units WHERE UnitId=" + str(Id) + ";"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    row = cursor.fetchone()  ### verify there is EXACTLY one hit!
+    return Unit(row[0], row[1])
+
+
+def unit_query(condition, connection):
+    "Return a list of objects resulting from a table query for CONDITION."
+    ### (Allow condition to be None, to fetch all records)
+    ### SELECT id FROM table WHERE condition
+    ### loop over rows, call constructor(self, Id) on each.
+
+
+# ------ INGREDIENTS -------------------------------------------------
 
 # a basic ingredient.  the db table is nothing but a name and unique id.
 class Ingredient:
-
-  def __init__(self, Name, IngId=-1):
+  
+  # Constructor:  all fields already available, just assemble.
+  def __init__(self, Id, Name):
+    self.IngId = IngId
     self.Name = Name
-    if IngId == -1:
-      # if user didn't pass an Id, try to look one up based on name
-      self.IngId = self.id_from_name(self, Name)
-
-  def id_from_name(self, Name):
-    """Look up the name of the ingredient in the db, return unique id
-    number.  If not found, return -1."""
-
-  def write_new_ingredient(self, Name):
-    """Write a new ingredient kind to the db, return new unique id."""
-
-
-# utility function:  get list of ALL unique ingredients in db
-def get_all_ingredients():
-  pass
-  ### run a sql query, build an Ingredient object for each hit.
 
 
 
@@ -63,15 +106,19 @@ class IngredientQuantity:
     self.Verb = Verb               # optional, like "refined"
 
 
+# ------ USERS -----------------------------------------------------
+
+### write someday.  low priority for the moment.
+
+
+# ------ RECIPES ---------------------------------------------------
+
 class Recipe:
 
-  # presumably the constructor is called when all the data is coming
-  # from a human source, like a huge CGI form.
-  
-  def __init__(self, Name, Rating=None, Instructions=None,
-               IngredientList = [],
-               Commentary=None, ServingHistory=None, Attribution=None):
-    self.RecipeId = -1  # unknown/invalid id
+  # Constructor:  all fields already available, just assemble.
+  def __init__(self, Id, Name, Rating, Instructions,
+               IngredientList, Commentary, ServingHistory, Attribution):
+    self.RecipeId = Id
     self.Name = Name
     self.Rating = Rating
     self.Instructions = Instructions
@@ -79,8 +126,4 @@ class Recipe:
     self.ServingHistory = ServingHistory
     self.Attribution = Attribution
     self.IngredientList = IngredientList  # list of IngredientQuantity objects
-    
 
-    
-
-  
