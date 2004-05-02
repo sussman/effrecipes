7fd4dcdc72b2353e226a3539cacbaacaf9af36ea
sussman <sussman@183c59d2-f21e-0410-bca8-81a7b2b58be6>
2004-05-02 04:03:07 +0000

    Actually get a class running in zope.

--- zope/trunk/RecipeCatalog.py
+++ zope/trunk/RecipeCatalog.py
@@ -4,14 +4,16 @@
 # (C) 2004 Ben Collins-Sussman under the GNU GPL.
 
 from OFS.SimpleItem import SimpleItem
-from Globals import DTMLFile, HTMLFile
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+#from Globals import HTMLFile
 
 
 # Global constructors for the module, referenced by __init__.py.
 # These methods allow the ZMI to "add" a new RecipeCatalog object.
 
-manage_addRecipeCatalogForm = DTMLFile("templates/addRecipeCatalogForm",
-                                       globals())
+manage_addRecipeCatalogForm = PageTemplateFile(
+  "templates/addRecipeCatalogForm.html",
+  globals())
 
 
 def manage_addRecipeCatalog(self, id, title, REQUEST):
@@ -21,7 +23,7 @@ def manage_addRecipeCatalog(self, id, title, REQUEST):
   newRecipeCatalog = RecipeCatalog(id, title)
 
   # this is the parent folder's method;  add the new object to parent.
-  self.__setObject(id, newRecipeCatalog)
+  self._setObject(id, newRecipeCatalog)
 
   # then have the object display itself.
   return self.manage_main(self, REQUEST)
@@ -45,7 +47,7 @@ class RecipeCatalog(SimpleItem):
   # An object that represents the main management view.  This is
   # effectively the "display" function of this class, although it
   # displays the object as a set of editable fields with a submit-button.
-  manage_main = HTMLFile("templates/mainRecipeCatalog.html", globals())
+  manage_main = PageTemplateFile("templates/mainRecipeCatalog.html", globals())
 
   # Create custom tabs in the ZMI to execute custom methods.
   # (We're overriding an inherited global from SimpleItem; it's a
@@ -67,9 +69,15 @@ class RecipeCatalog(SimpleItem):
 
     self.title = title
     # After changing the object, have the object redisplay itself.
-    return self.manage_main(self, REQUEST)
+    #return self.manage_main(self, REQUEST)
+
+  # a read accessor
+  def getTitle(self):
+    "Return the title of a RecipeCatalog."
+    return self.title
+
 
   # Web Presentation Methods
 
   # a method name Zope expects:
-  index_html = HTMLFile("templates/indexRecipeCatalog.html", globals())
+  index_html = PageTemplateFile("templates/indexRecipeCatalog.html", globals())
