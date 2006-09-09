72817f75b6debd369e2e0017cd304097d05dba2a
sussman <sussman@183c59d2-f21e-0410-bca8-81a7b2b58be6>
2006-09-09 19:15:51 +0000

    Update latest progress.

--- django/trunk/recipes/views.py
+++ django/trunk/recipes/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+
+# From the tutorial: every URL callback must either return an
+# HttpResponse object containing page content, or must raise an http
+# exception.
+
+
+# Front page of site
+def index(request):
+    return HttpResponse("Hello, world. You're at the recipe index.")
+
+
+# Read-only view of a single recipe
+def detail(request, recipe_id):
+    return HttpResponse("You're looking at recipe %s." % recipe_id)
+
