60238c9a7e242fccc9a15848957a9295e0ba6ee1
sussman <sussman@183c59d2-f21e-0410-bca8-81a7b2b58be6>
2006-09-18 03:46:22 +0000

    Check in further progress.

--- django/trunk/recipes/views.py
+++ django/trunk/recipes/views.py
@@ -11,7 +11,7 @@ from effrecipes.recipes.models import *
 # Front page of site
 def index(request):
   num_recipes = Recipe.objects.count()
-  cats = RecipeCategory.objects.all()
+  cats = RecipeCategory.objects.filter(is_primary__exact=True)
   return render_to_response('frontpage.html', {'recipecount' : num_recipes,
                                                'cats' : cats })
 
@@ -20,13 +20,25 @@ def index(request):
 def recipe_search(request):
   term = request.POST['searchtext']
 
-  ## TODO: search over ingredient lists too!!
+  # search for the term in Recipe fields first
   recipes = Recipe.objects.filter(
     Q(title__contains=term)
     | Q(instructions__contains=term)
     | Q(servinghistory__contains=term)
     | Q(attribution__contains=term)
     )
+
+  # Then search through Ingredients
+  ingredients = Ingredient.objects.filter(
+    Q(unit__id__contains=term)
+    | Q(food__id__contains=term)
+    | Q(verb__contains=term)
+    )
+  for ing in ingredients:
+    r = ing.recipe
+    if r not in recipes:
+      recipes.append(r)
+
   return render_to_response('searchresults.html', { 'recipes' : recipes } )
 
 
@@ -38,9 +50,9 @@ def show_category(request, category_id):
 
 # Read-only view of a single recipe
 def detail(request, recipe_id):
-  ings = Ingredient.objects.filter(recipe__id__exact=recipe_id)
-  cats = RecipeCategory.objects.filter(recipe__id__exact=recipe_id)
   r = get_object_or_404(Recipe, pk=recipe_id)
+  ings = Ingredient.objects.filter(recipe__id__exact=recipe_id)
+  cats = RecipeCategory.objects.filter(recipes__exact=recipe_id)
   return render_to_response('details.html', {'recipe' : r,
                                              'inglist' : ings,
                                              "catlist" : cats} )
